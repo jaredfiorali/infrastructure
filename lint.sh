26a7b39d165e8a366951ab544b3a1b3dde2d8dd0
Jared Fiorali <jared@JaredsMacStudio.fiora.li>
2025-04-26 13:02:46 -0400

    Added a helm linter. Also minor PrometheusRules changes

---
+++ lint.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+clear
+for chart in charts/fiorali/*; do
+  # Skip: Charts.yaml, values.yaml, transmission-init.yaml, and templates folder
+  if [ $chart == "charts/fiorali/Chart.yaml" ] || [ $chart == "charts/fiorali/values.yaml" ] || [ $chart == "charts/fiorali/transmission-init.yaml" ] || [ $chart == "charts/fiorali/templates" ]; then
+    echo "Skipping $chart"
+  else
+    echo "Linting ${chart}"
+    helm lint --quiet charts/fiorali -f charts/fiorali/values.yaml -f $chart;
+  fi
+done
