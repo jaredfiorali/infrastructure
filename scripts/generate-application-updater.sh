44ec99b77f2eccb9f1f13a5c6f20d38dde5ed862
Jared Fiorali <jaredfiorali@gmail.com>
2025-06-13 10:28:38 -0400

    Fix boolean in updater script

--- scripts/generate-application-updater.sh
+++ scripts/generate-application-updater.sh
@@ -3,15 +3,13 @@
 APPLICATION="$1"
 IMAGE="$2"
 
-echo "
-  annotations:
+echo "  annotations:
     argocd-image-updater.argoproj.io/git-branch: main
     argocd-image-updater.argoproj.io/git-repository: git@github.com:jaredfiorali/infrastructure.git
     argocd-image-updater.argoproj.io/image-list: myalias=$IMAGE:latest
-    argocd-image-updater.argoproj.io/myalias.force-update: "true"
+    argocd-image-updater.argoproj.io/myalias.force-update: 'true'
     argocd-image-updater.argoproj.io/myalias.helm.image-name: image.repository
     argocd-image-updater.argoproj.io/myalias.helm.image-tag: image.tag
     argocd-image-updater.argoproj.io/myalias.update-strategy: digest
     argocd-image-updater.argoproj.io/write-back-method: git
-    argocd-image-updater.argoproj.io/write-back-target: helmvalues:charts/fiorali/$APPLICATION.yaml
-" > scripts/tmp/updater.yaml
+    argocd-image-updater.argoproj.io/write-back-target: helmvalues:charts/fiorali/$APPLICATION.yaml" > scripts/tmp/updater.yaml
