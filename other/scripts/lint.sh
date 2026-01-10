5af93681f7f05e001d342e73e010a836860c8055
Jared Fiorali <jaredfiorali@gmail.com>
2026-01-10 14:54:59 -0500

    Configmap fix for argocd

--- other/scripts/lint.sh
+++ other/scripts/lint.sh
@@ -2,13 +2,14 @@
 
 clear
 for chart in charts/fiorali/*; do
+  chart_name=$(basename $chart .yaml)
   # Skip: Charts.yaml, values.yaml, and templates folder
   if [ $chart == "charts/fiorali/Chart.yaml" ] || [ $chart == "charts/fiorali/values.yaml" ] || [ $chart == "charts/fiorali/templates" ]; then
-    echo "Skipping $chart"
+    echo "Skipping $chart_name"
   else
-    echo "Linting ${chart}"
+    echo "Linting ${chart_name}"
     helm lint --quiet charts/fiorali -f charts/fiorali/values.yaml -f $chart;
+    yamllint  -c other/scripts/.yamllint.yaml $chart_name;
+    other/scripts/test-chart.sh $chart_name && yamllint  -c other/scripts/.yamllint.yaml "other/scripts/tmp/${chart_name}_debug.yaml";
   fi
 done
-
-yamllint  -c other/scripts/.yamllint.yaml charts/fiorali
--- other/scripts/test-chart.sh
+++ other/scripts/test-chart.sh
@@ -1,4 +1,3 @@
 #/bin/bash
 
-clear
-helm template charts/fiorali --values=charts/fiorali/values.yaml --values=charts/fiorali/"$1".yaml --debug > debug.yaml
+helm template charts/fiorali --values=charts/fiorali/values.yaml --values="charts/fiorali/${1}.yaml" --debug > "other/scripts/tmp/${1}_debug.yaml"
